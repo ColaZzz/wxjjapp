8cf454e74d80155aa363488c920871a41b985e7d
ColaZzz <ljlhyyc@qq.com>
2018-11-16 17:26:52 +0800

    friday

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,7 +2,7 @@
 App({
   onLaunch: function() {
     let that = this
-
+    let username
     this.url = 'http://jjapp.test/api/'
     // this.url = 'https://togetherwoh.com/api/'
     this.estateImgPrefix = 'http://jjapp.test/uploads/estates/'
@@ -17,11 +17,29 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
+              username = res.userInfo.nickName
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
                 this.userInfoReadyCallback(res)
+                // 在获取用户信息后的回调中进行登录，即没有获取到用户信息的时候还没有获取登录态
+                // 用户登录
+                wx.login({
+                  success(res) {
+                    wx.request({
+                      url: that.url + 'login',
+                      method: 'POST',
+                      data: {
+                        code: res.code,
+                        username: username
+                      },
+                      success(result) {
+                        let session_3rd = result.data.data
+                        wx.setStorageSync('token', session_3rd)
+                      }
+                    })
+                  }
+                })
               }
             }
           })
@@ -29,22 +47,7 @@ App({
       }
     })
 
-    // 用户登录
-    wx.login({
-      success(res) {
-        wx.request({
-          url: that.url + 'login',
-          method: 'POST',
-          data: {
-            code: res.code
-          },
-          success(result) {
-            let session_3rd = result.data.data
-            wx.setStorageSync('token', session_3rd)
-          }
-        })
-      }
-    })
+    
   },
   globalData: {
     userInfo: null
