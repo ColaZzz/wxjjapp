efc35a4fcba7e250f66fd4f4edcb11986d5a6bff
ColaZzz <ljlhyyc@qq.com>
2019-04-01 14:33:24 +0800

    二维码更改

--- miniprogram/pages/block_qrcode/block_qrcode.js
+++ miniprogram/pages/block_qrcode/block_qrcode.js
@@ -6,7 +6,9 @@ Page({
    */
   data: {
     code: '',
-    datetime: ''
+    datetime: '',
+    value: 'https://github.com/skyvow/wux',
+    fgColor: 'black',
   },
 
   /**
@@ -34,5 +36,37 @@ Page({
     wx.navigateBack({
       delta: 1
     })
-  }
+  },
+
+  /**
+   * 二维码的代码，没有去研究
+   */
+  bindinput(e) {
+    const value = e.detail.value
+    const fgColor = this.randomColor()
+
+    this.setData({
+      value,
+      fgColor,
+    })
+  },
+  previewImage() {
+    // 在自定义组件下，当前组件实例的 this，以操作组件内 <canvas> 组件
+    const that = this.selectComponent('#qrcode')
+
+    wx.canvasToTempFilePath({
+      canvasId: 'wux-qrcode',
+      success: (res) => {
+        wx.previewImage({
+          urls: [res.tempFilePath]
+        })
+      }
+    }, that)
+  },
+  randomColor() {
+    const colorStr = Math.floor(Math.random() * 0xFFFFFF).toString(16).toUpperCase()
+    const length = colorStr.length
+    const prefixStr = `000000`.substring(0, 6 - colorStr.length)
+    return `#${prefixStr}${colorStr}`
+  },
 })
